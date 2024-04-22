481ee9dba0f74594cc3ef2721ec1ac3f9771dedc
KeimaSenpai <hg4052650@gmail.com>
2024-04-22 17:05:59 -0400

    1.0.5

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import asyncio
 from minecraft_launcher.menu import menu_I
 
 
+async def inicio():
+    await menu_I()
+
 if __name__ == '__main__':
-    asyncio.run(menu_I())
+    asyncio.run(inicio())
